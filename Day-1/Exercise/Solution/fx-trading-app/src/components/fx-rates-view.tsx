044353196689f4d91571786ae546eb4a0a3064e8
Dana Munteanu <dana.maria.munteanu@gmail.com>
2020-04-16 15:05:09 +0300

    Day 1 and Day 2 Exercises updated with exercise 9

---
+++ Day-1/Exercise/Solution/fx-trading-app/src/components/fx-rates-view.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+interface Props {
+}
+interface State {
+}
+
+class FxRatesView extends Component < Props, State > {
+    state = {}
+
+    render() {
+        return (
+            <div>
+                fx rates view
+            </div>
+        )
+    }
+}
+
+export default FxRatesView
---
+++ Day-2/Exercise/Code/fx-trading-app/src/components/widget.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+interface Props {
+}
+interface State {
+}
+
+class Widget extends Component < Props, State > {
+    state = {}
+
+    render() {
+        return (
+            <div>
+                widget
+            </div>
+        )
+    }
+}
+
+export default Widget
